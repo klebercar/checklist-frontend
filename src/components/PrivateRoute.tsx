a37052bcb4c5555ebf160369932ae16ef4a6fa8d
Kleber Ribeiro Carneiro <kleber.carneiro@dataprev.gov.br>
2025-10-02 15:17:07 -0300

    Revert "feat(frontend): axios + JWT + serviços + hook de auth (+ proxy opcional)"
    This reverts commit e8b34bed2924443facf816f79d5729f712c681ab.

--- src/components/PrivateRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { getAuthHeader } from "../lib/api";
-
-export default function PrivateRoute() {
-  const isAuth = !!getAuthHeader();
-  return isAuth ? <Outlet /> : <Navigate to="/login" replace />;
-}
